6e968309e8374ae8940839102b652a299031afbe
Pranay <praveenpranay07@gmail.com>
2025-12-19 12:28:28 +0530

    Initial setup: React + Vite with Tailwind CSS

---
+++ Context-api/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-6xl text-gray-800 text-bold">Welcome To Theme Changing App</h1>
+    <button className=" text-md bg-slate-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Change Theme</button>
+    </>
+  )
+}
+
+export default App
